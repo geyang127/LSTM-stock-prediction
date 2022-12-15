fa560b4d01a3a84691f4c0f452e1752918b7232f
ge-yang <gy2297@columbia.edu>
2022-12-14 22:19:17 -0500

    Update network.py

--- network.py
+++ network.py
@@ -46,3 +46,21 @@ class LSTMModel(tf.keras.Model):
         output = self.dropout_3(output)
         output = self.ou(output)
         return output
+
+
+class SingleLSTMModel(tf.keras.Model):
+    def __init__(self, input_shape=8, output_shape=1, hidden_shape=None):
+        super(SingleLSTMModel, self).__init__()
+        self.lstm_1 = layers.LSTM(input_shape, activation='relu', return_sequences=True, kernel_regularizer=keras.regularizers.l2(0.01))
+        self.dropout_1 = layers.Dropout(0.2)
+        self.dense = layers.Dense(16, activation='relu', kernel_initializer='random_normal', kernel_regularizer=keras.regularizers.l2(0.01))
+        self.dropout_3 = layers.Dropout(0.2)
+        self.ou = layers.Dense(1)
+
+    def call(self, input_tensor, training=False):
+        output =self.lstm_1(input_tensor)
+        output = self.dropout_1(output)
+        output = self.dense(output)
+        output = self.dropout_3(output)
+        output = self.ou(output)
+        return output
