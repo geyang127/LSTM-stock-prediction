059731dd5b38ff4899d040423f0e5f1a3c9a0e01
ge-yang <gy2297@columbia.edu>
2022-12-14 22:18:34 -0500

    Update invest_agent.py

--- invest_agent.py
+++ invest_agent.py
@@ -1,12 +1,31 @@
-model.evaluate(test_ds)
-y_pred = model.predict(x_test)
+
+import pandas as df
+import matplotlib.pyplot as plt
+def agent_trade(model,x_test,test_ds,y_test,window_size):
+    model.evaluate(test_ds)
+    y_pred = model.predict(x_test)
+    df_time = df.index[-len(y_test):]
+    is_hold=0
+    reward = 0.0
+    return_list = []
+    reward_list=[]
+    for i in range(len(window_size,y_pred)):
+        return_list.append(y_test[i]-y_test[i-window_size])
+        if  (y_pred[i]-y_pred[i-window_size])/y_pred[i-window_size]<=0.02:
+            if is_hold ==1:
+                reward+=(y_test[i]-y_test[i-window_size])/y_test[i-window_size]
+                reward_list.append(reward)
+                is_hold =0
+        else:
+            is_hold=1
+    return return_list,reward_list
+
 df_time = df.index[-len(y_test):]
 fig = plt.figure(figsize=(10, 5))
 axes = fig.add_subplot(111)
-axes.plot(df_time, y_test, 'b-', label='stacked GRU')
-history = model.fit(train_ds, epochs=30, validation_data=val_ds)
-axes.plot(df_time, y_new, 'y-', label='hold and sell')
-axes.plot(df_time, y_pred, 'r--', label='stacked LSTM')
+axes.plot(df_time, gru_reward, 'b-', label='stacked GRU')
+axes.plot(df_time, reward_baseline 'y-', label='hold and sell')
+axes.plot(df_time, lstm_reward), 'r--', label='stacked LSTM')
 axes.set_xticks(df_time[::50])
 plt.legend()
 plt.grid()
